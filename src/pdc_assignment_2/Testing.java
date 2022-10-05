d50631e933f7dbb93015d5df4c1eff313dc844a1
Derick Dela Cruz <derickbdc8@gmail.com>
2022-10-06 12:27:32 +1300

    add main function

---
+++ src/pdc_assignment_2/Testing.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pdc_assignment_2;
+
+/**
+ *
+ * @author DDC
+ */
+public class Testing {
+    public static void main(String[] args) {
+        
+    }
+    
+}
